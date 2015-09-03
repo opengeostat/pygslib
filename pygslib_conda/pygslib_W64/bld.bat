d60eda0beba289b20ce6f718eb7717f6e5452aa2
opengeostat <adrian.martinez@opengeostat.com>
2015-09-03 01:48:50 -0700

    adding conda skeletonfor python 2.7 64 bits for windows and linux

---
+++ pygslib_conda/pygslib_W64/bld.bat
@@ -0,0 +1,10 @@
+conda config --add channels https://conda.binstar.org/ioos
+conda install -c https://conda.binstar.org/ioos mingwpy
+"%PYTHON%" setup.py config --compiler=mingw32 build --compiler=mingw32 install
+if errorlevel 1 exit 1
+
+:: Add more build steps here, if they are necessary.
+
+:: See
+:: http://docs.continuum.io/conda/build.html
+:: for a list of environment variables that are set during the build process.
