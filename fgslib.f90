401e9e6a01411f411128c373539b8b46bef59f63
opengeostat <adrian.martinez@opengeostat.com>
2015-10-02 19:05:28 -0700

    adding back transformation of nscore data

--- fgslib.f90
+++ fgslib.f90
@@ -600,6 +600,7 @@ subroutine locate(xx,n,is,ie,x,j)
     ! -----------------------------------------------------------------------
 
     real*8, intent(in), dimension(n) :: xx
+    real*8 :: x
  
     ! Initialize lower and upper methods:
  
@@ -1200,6 +1201,7 @@ subroutine gauinv(p,xp,ierr)
 
 
 	!-----------------------------------------------------------------------
+    real :: xp
     real*8 :: p0,p1,p2,p3,p4,q0,q1,q2,q3,q4,y,pp,lim,p
     save   p0,p1,p2,p3,p4,q0,q1,q2,q3,q4,lim
 
@@ -1306,7 +1308,7 @@ subroutine dlocate(xx,n,is,ie,x,j)
     return
 end subroutine dlocate
 
-real function gcum(x)
+real*8 function gcum(x)
 	!-----------------------------------------------------------------------
 
 	! Evaluate the standard normal cdf given a normal deviate x.  gcum is
@@ -1315,6 +1317,9 @@ real function gcum(x)
 
 
 	!-----------------------------------------------------------------------
+    
+    real*8 :: x, z, e2
+    
     z = x
     if(z < 0.) z = -z
     t    = 1./(1.+ 0.2316419*z)
@@ -1330,6 +1335,28 @@ real function gcum(x)
     gcum = 1.0 - gcum
     return
 end function gcum
+
+real*8 function powint(xlow,xhigh,ylow,yhigh,xval,pow)
+	!-----------------------------------------------------------------------
+
+	! Power interpolate the value of y between (xlow,ylow) and (xhigh,yhigh)
+	!                 for a value of x and a power pow.
+
+	!-----------------------------------------------------------------------
+    real*8 :: EPSLON, xlow,xhigh,ylow,yhigh,xval, pow
+
+	EPSLON=1.0e-20
+
+    if((xhigh-xlow) < EPSLON) then
+        powint = (yhigh+ylow)/2.0
+    else
+        powint = ylow + (yhigh-ylow)* &
+        (((xval-xlow)/(xhigh-xlow))**pow)
+    end if
+
+    return
+
+end function powint
 
 
 !*********************************************************************************
@@ -3728,8 +3755,8 @@ subroutine nscore(va, nd, transin, transout, nt, getrank , nsc)
 	real*8 :: EPSLON
     real*8 :: ixv (12)
 	integer :: KORDEI, MAXOP1, i, j
-    real*8 :: vrg,acorni, p, yy, vrr
-	real :: gcum, pp
+    real*8 :: vrg,acorni, p, yy, vrr, gcum
+	real ::  pp
     
 	doubone=1.0
     EPSLON=1.0e-6
@@ -3771,6 +3798,218 @@ subroutine nscore(va, nd, transin, transout, nt, getrank , nsc)
 	end do
 
 end subroutine nscore
+
+real*8 function backtr(vrgs,nt,vr,vrg,zmin,zmax,ltail,ltpar, utail,utpar)
+	!-----------------------------------------------------------------------
+
+	!           Back Transform Univariate Data from Normal Scores
+	!           *************************************************
+
+	! This subroutine backtransforms a standard normal deviate from a
+	! specified back transform table and option for the tails of the
+	! distribution.  Call once with "first" set to true then set to false
+	! unless one of the options for the tail changes.
+
+
+
+	! INPUT VARIABLES:
+
+	!   vrgs             normal score value to be back transformed
+	!   nt               number of values in the back transform tbale
+	!   vr(nt)           original data values that were transformed
+	!   vrg(nt)          the corresponding transformed values
+	!   zmin,zmax        limits possibly used for linear or power model
+	!   ltail            option to handle values less than vrg(1):
+	!   ltpar            parameter required for option ltail
+	!   utail            option to handle values greater than vrg(nt):
+	!   utpar            parameter required for option utail
+
+
+
+	!-----------------------------------------------------------------------
+
+    real*8, dimension(nt) :: vr, vrg
+    real*8 ::    ltpar,utpar,lambda, EPSLON, vrgs,zmin,zmax, powint, &
+				 cdflo, cdfbt, cpow, cdfhi, gcum
+    integer ::   ltail,utail, nt
+
+	EPSLON=1.0e-20
+
+	! Value in the lower tail?    1=linear, 2=power, (3 and 4 are invalid):
+
+    if(vrgs <= vrg(1)) then
+        backtr = vr(1)
+        cdflo  = gcum(vrg(1))
+        cdfbt  = gcum(vrgs)
+        if(ltail == 1) then
+            backtr = powint(dble(0.0),cdflo,zmin,vr(1),cdfbt,dble(1.0))
+        else if(ltail == 2) then
+            cpow   = 1.0 / ltpar
+            backtr = powint(dble(0.0),cdflo,zmin,vr(1),cdfbt,cpow)
+        endif
+    
+    ! Value in the upper tail?     1=linear, 2=power, 4=hyperbolic:
+    
+    else if(vrgs >= vrg(nt)) then
+        backtr = vr(nt)
+        cdfhi  = gcum(vrg(nt))
+        cdfbt  = gcum(vrgs)
+        if(utail == 1) then
+            backtr = powint(cdfhi,dble(1.0),vr(nt),zmax,cdfbt,dble(1.0))
+        else if(utail == 2) then
+            cpow   = 1.0 / utpar
+            backtr = powint(cdfhi,dble(1.0),vr(nt),zmax,cdfbt,cpow)
+        else if(utail == 4) then
+            lambda = (vr(nt)**utpar)*(1.0-gcum(vrg(nt)))
+            backtr = (lambda/(1.0-gcum(vrgs)))**(1.0/utpar)
+        endif
+    else
+    
+    ! Value within the transformation table:
+    
+        call locate(vrg,nt,1,nt,vrgs,j)
+        j = max(min((nt-1),j),1)
+        backtr = powint(vrg(j),vrg(j+1),vr(j),vr(j+1),vrgs,dble(1.0))
+    endif
+
+    return 
+
+end function backtr
+
+
+!---------------------------------------------------------------------------
+!     Subroutine bacnscore (this is the bactr program for bac transformation of normal score variables)
+!---------------------------------------------------------------------------
+subroutine bacnscore(vnsc, nd, transin, transout, nt, ltail,utail, ltpar,utpar, zmin,zmax, getrank, va, error)
+	!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	!                                                                      %
+	! Copyright (C) 1996, The Board of Trustees of the Leland Stanford     %
+	! Junior University.  All rights reserved.                             %
+	!                                                                      %
+	! The programs in GSLIB are distributed in the hope that they will be  %
+	! useful, but WITHOUT ANY WARRANTY.  No author or distributor accepts  %
+	! responsibility to anyone for the consequences of using them or for   %
+	! whether they serve any particular purpose or work at all, unless he  %
+	! says so in writing.  Everyone is granted permission to copy, modify  %
+	! and redistribute the programs in GSLIB, but only under the condition %
+	! that this notice and the above copyright notice remain intact.       %
+	!                                                                      %
+	!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	!-----------------------------------------------------------------------
+
+	!                     Gaussian Back Transformation
+	!                     ****************************
+
+	! PROGRAM NOTES:
+
+	!  1. ltail, utail options: 1=linear interpolation, 2=power model
+	!     interpolation, and 4=hyperbolic model interpolation (only for
+	!     upper tail)
+
+
+
+	! EXTERNAL REFERENCES:
+
+	!   gcum     Inverse of Gaussian cumulative distribution function
+	!   locate   locate a position in an array
+	!   powint   power law interpolation
+
+
+
+	!-----------------------------------------------------------------------
+
+
+
+	implicit none
+
+	! input 
+	integer, intent(in) :: nd, nt
+	logical, intent(in) ::  getrank	
+	integer, intent(in) :: ltail,utail
+	real*8,  intent(in) :: ltpar,utpar, zmin,zmax
+	real*8, intent(in),   dimension(nd) :: vnsc
+	real*8, intent (in), dimension (nt) :: transin, transout
+
+
+	! output
+	real*8, intent(out), dimension(nd):: va
+	integer, intent(out) :: error
+	
+	! internal 
+	real*8 :: backtr, p, bac
+	real*8 :: EPSLON
+	integer :: i, ierr
+
+	
+
+	real*8, dimension(nd) :: vr,vrg
+
+
+	EPSLON=0.00001
+
+
+	! Check for error situation:
+
+    if(ltail /= 1 .AND. ltail /= 2) then
+        error = 10 ! 'ERROR invalid lower tail option only allow 1 or 2 - see manual '
+        return
+    endif
+    if(utail /= 1 .AND. utail /= 2 .AND. utail /= 4) then
+        error = 11 ! 'ERROR invalid upper tail option, only allow 1,2 or 4 - see manual '
+        return
+    endif
+    if(utail == 4 .AND. utpar < 1.0) then
+        error = 12 ! 'ERROR invalid power for hyperbolic tail, must be greater than 1.0!'
+        return
+    endif
+
+	! Read in the transformation table:
+
+	do i=1, nt
+        vr(i)  = transin(i)
+        vrg(i) = transout(i)
+        if(i > 1) then
+            if(vr(i) < vr(i-1) .OR. vrg(i) < vrg(i-1)) then
+                error = 100 ! 'ERROR transformation table must be monotonic increasing! '
+                return 
+            endif
+        endif
+	end do
+
+	! Check for error situation:
+
+    if(utail == 4 .AND. vr(nt) <= 0.0) then
+        error = 200   ! 'ERROR can not use hyperbolic tail with negative values! - see manual '
+        return
+    endif
+    if(zmin > vr(1)) then
+        error = 210   ! 'ERROR zmin should be no larger than the first entry in the transformation table '
+        return
+    endif
+    if(zmax < vr(nt)) then
+        error = 220   ! 'ERROR zmax should be no less than the last entry in the transformation table '
+        return
+    endif
+
+	! Now do the transformation using backtr function
+
+	do i=1, nd
+
+		if(getrank) then
+		    p = vnsc(i)
+		    call gauinv(p,vnsc(i),ierr)
+		end if
+		bac = backtr(vnsc(i),nt,vr,vrg,zmin,zmax,ltail,ltpar,utail,utpar)
+		if(bac < zmin) bac = zmin
+		if(bac > zmax) bac = zmax
+        
+		va(i)=bac
+
+	end do
+    
+    return
+
+end subroutine bacnscore
 
 !---------------------------------------------------------------------------
 !     Subroutine declus (this is the declus program for declustering)
@@ -4551,6 +4790,7 @@ subroutine probplt( iwt, nd, va, wt,  &
     endif
 
 end subroutine probplt
+
 
 
 !***********************************************************************
