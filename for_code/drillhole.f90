4be0fcf0bcc3c37a7bf24987a7e71a5fece95935
opengeostat <adrian.martinez@opengeostat.com>
2015-10-23 10:32:44 -0700

    work on desurvey/drillholes

--- for_code/drillhole.f90
+++ for_code/drillhole.f90
@@ -129,107 +129,42 @@ subroutine interp_ang1D(azm1,dip1,azm2,dip2,len12,d1, azm,dip)
 end subroutine interp_ang1D
 
 
-!-----------------------------------------------------------------------
-! functions to put in assay x,y,z from collar
-!----------------------------------------------------------------------- 
-subroutine collr2tbl(nc,idc,xc,yc,zc,nt,idt,xt,yt,zt)
+!!-----------------------------------------------------------------------
+!! functions to put in table x,y,z from collar (need this for desurvey)
+!!----------------------------------------------------------------------- 
+!subroutine collr2tbl(nc,idc,xc,yc,zc,nt,idt,xt,yt,zt)
     
-    ! this works only with sorted arrays
+!    ! this works only with sorted arrays
     
-    implicit none
+!    implicit none
     
-    !input
-    integer, intent(in) :: nc,nt
-    integer, intent(in), dimension(nc) :: idc
-    real, intent(in), dimension(nc) :: xc,yc,zc
-    integer, intent(in), dimension(nt) :: idt
+!    !input
+!    integer, intent(in) :: nc,nt
+!    integer, intent(in), dimension(nc) :: idc
+!    real, intent(in), dimension(nc) :: xc,yc,zc
+!    integer, intent(in), dimension(nt) :: idt
     
-    !output
-    real, intent(out), dimension(nt) :: xt,yt,zt
+!    !output
+!    real, intent(out), dimension(nt) :: xt,yt,zt
     
-    !internal
-    integer :: i,j,actualc
+!    !internal
+!    integer :: i,j,actualc
     
-    actualc=0
-    do i=0,nt
-        ! find the first collar similar to i starting from last found
-        do j=actualc,nc   ! actual will skip already found
-            if (idt(i)==idc(j)) then
-                xt(i)=xc(j)
-                yt(i)=yc(j)
-                zt(i)=zc(j)
-                actualc = j 
-                exit
-            end if
-        end do  
-    end do
+!    actualc=0
+!    do i=0,nt
+!        ! find the first collar similar to i starting from last found
+!        do j=actualc,nc   ! actual will skip already found
+!            if (idt(i)==idc(j)) then
+!                xt(i)=xc(j)
+!                yt(i)=yc(j)
+!                zt(i)=zc(j)
+!                actualc = j 
+!                exit
+!            end if
+!        end do  
+!    end do
     
-end subroutine collr2tbl
-
-!-----------------------------------------------------------------------
-! functions to put in assay az, dip from survey
-!----------------------------------------------------------------------- 
-subroutine surv2tbl(ns,ids,lengs,azs,dips,nt,idt,fromt,midt,tot, &
-                    azbs,dipbs,azms,dipms,azes,dipes)
-    
-    ! this works only with sorted arrays
-    
-    implicit none
-    
-    !input
-    integer, intent(in) :: ns,nt
-    integer, intent(in), dimension(ns) :: ids
-    real, intent(in), dimension(ns) :: azs,dips,lengs
-    integer, intent(in), dimension(nt) :: idt
-    real, intent(in), dimension(nt) :: fromt,midt,tot
-    
-    !output (anges at begin, mid and end interval)
-    real, intent(out), dimension(nt) :: azbs,dipbs,azms,dipms,azes,dipes
-    
-    !internal
-    integer :: i,j,actualt
-    real :: a, b, azm1,dip1,azm2,dip2,len12,d1
-    
-    actualt=0
-    do i=1,ns-1
-        ! get the segment [a-b] to test intervals in the table
-        if (ids(i)==ids(i+1)) then
-            a=lengs(i)
-            b=lengs(i+1)
-            azm1 = azs(i)
-            dip1 = dips(i)
-            azm2 = azs(i+1)
-            dip2 = dips(i+1)
-            len12 = lengs(i+1)-lengs(i)
-        else
-            cycle
-        end if
-        
-        ! now loop in the table to test the interval
-        do j=actualt,nt  
-            ! if we are in the right drillhole
-            if (ids(i)==idt(j)) then
-                ! test if we are in the interval, interpolate angles
-                if (fromt(j)>=a .AND. fromt(j)<=b ) then                     
-                    d1= fromt(j)- a
-                    call interp_ang1D(azm1,dip1,azm2,dip2,len12,d1,azbs(j),dipbs(j))
-                end if 
-                if (midt(j)>=a  .AND. midt(j)<=b ) then
-                    d1= midt(j)- a
-                    call interp_ang1D(azm1,dip1,azm2,dip2,len12,d1,azms(j),dipms(j))
-                end if
-                if (tot(j)>=a   .AND. tot(j)<=b ) then
-                    d1= tot(j)- a
-                    call interp_ang1D(azm1,dip1,azm2,dip2,len12,d1,azes(j),dipes(j))
-                end if
-                actualt = j 
-                cycle
-            end if
-        end do  
-    end do
-    
-end subroutine surv2tbl
-
+!end subroutine collr2tbl
 
 !-----------------------------------------------------------------------
 ! functions to desurvey assay tables (assuming you have x,y,z,az,dip)
@@ -261,45 +196,120 @@ subroutine dsmincurb(len12,az1,dip1,az2,dip2,dz,dn,de)
     a2 = az2 * DEG2RAD
     
     ! calculate the dog-leg (dl) and the Ratio Factor (rf)
-    dl = acos(cos(i2-i1)-sin(i1)*sin(i2)*(1-cos(a2-a1)))
-    rf = 2*tan(dl/2)/dl 
+    dl = acos(cos(i2-i1)-sin(i1)*sin(i2)*(1-cos(a2-a1))) !
+    
+    if (dl/=0) then
+        rf = 2*tan(dl/2)/dl  ! minimum curvature
+    else
+        rf=1 ! balanced tangential
+    end if
     
     
     dz = 0.5*len12*(cos(i1)+cos(i2))*rf
     dn = 0.5*len12*(sin(i1)*cos(a1)+sin(i2)*cos(a2))*rf
-    de = 0.5*len12*(sin(i1)*sin(a1)+sin(i2)*sin(a2))
+    de = 0.5*len12*(sin(i1)*sin(a1)+sin(i2)*sin(a2))*rf
     
     return
     
 end subroutine dsmincurb
 
 
-subroutine dstang(len12,az1,dip1,az2,dip2,dz,dn,de)
-    ! using formulas in http://www.cgg.com/data//1/rec_docs/2269_MinimumCurvatureWellPaths.pdf
+!subroutine dstang(len12,az1,dip1,az2,dip2,dz,dn,de)
+!    ! using formulas in http://www.cgg.com/data//1/rec_docs/2269_MinimumCurvatureWellPaths.pdf
     
-    ! here we calculate the deltas only... 
+!    ! here we calculate the deltas only... 
+    
+!    implicit none
+    
+!    ! input
+!    real, intent(in) :: len12,az1,dip1,az2,dip2  ! az2,dip2  not used but keep it for other methods
+    
+!    ! output
+!    real, intent(out) :: dz,dn,de 
+
+
+!    ! internal 
+!    real :: i1, a1, DEG2RAD
+    
+!    DEG2RAD=3.141592654/180.0
+    
+!    i1 = (90 - dip1) * DEG2RAD
+!    a1 = az1 * DEG2RAD
+    
+!    dz = len12*cos(i1)
+!    dn = len12*sin(i1)*cos(a1)
+!    de = len12*sin(i1)*sin(a1)
+    
+!    return
+    
+!end subroutine dstang
+
+
+
+subroutine desurv1dh(ns,lengs,azs,dips,xc,yc,zc,lpt, &
+                    azt,dipt,xt,yt,zt)
+    ! desrurvey point in a drillhole trace and located at a depth lpt
     
     implicit none
     
-    ! input
-    real, intent(in) :: len12,az1,dip1,az2,dip2  ! az2,dip2  not used but keep it for other methods
+    !input
+    integer, intent(in) :: ns
+    real, intent(in), dimension(ns) :: azs,dips,lengs  ! this zc... is from collar
+    real, intent(in) :: lpt
+    real, intent(in) :: xc,yc,zc
     
-    ! output
-    real, intent(out) :: dz,dn,de 
+    !output (anges at begin, mid and end interval)
+    real, intent(out) :: azt,dipt,xt,yt,zt
+    
+    !internal
+    integer :: i,j
+    real :: a,b,azm1,dip1,azm2,dip2,len12,d1, &
+            EPSLON,xa,ya,za,xb,yb,zb,dz,dn,de
+    
+    EPSLON=1.0e-4
 
-
-    ! internal 
-    real :: i1, a1, DEG2RAD
+    do i=1,ns-1
+        ! get the segment [a-b] to test interval
+        
+        a=lengs(i)
+        b=lengs(i+1)
+        azm1 = azs(i)
+        dip1 = dips(i)
+        azm2 = azs(i+1)
+        dip2 = dips(i+1)
+        len12 = lengs(i+1)-lengs(i)
+        ! desurvey at survey table
+        if (lengs(i)>=-EPSLON .AND. lengs(i)<=EPSLON ) then !zero interval?
+            xa = xc
+            ya = yc
+            za = zc
+            ! desurvey interval at zurvey... table
+            call dsmincurb(len12,azm1,dip1,azm2,dip2,dz,dn,de)
+            xb = xa+de
+            yb = ya+dn
+            zb = za-dz
+        else
+            xa = xb
+            ya = yb
+            za = zb
+            ! desurvey interval at zurvey... table
+            call dsmincurb(len12,azm1,dip1,azm2,dip2,dz,dn,de)
+            xb = xa+de
+            yb = ya+dn
+            zb = za-dz
+        end if
+        
+        ! test if we are in the interval, interpolate angles
+        if (lpt>=a  .AND. lpt<b ) then
+            d1= lpt- a
+            call interp_ang1D(azm1,dip1,azm2,dip2,len12,d1,azt,dipt)
+            !desurvey at interval table 
+            call dsmincurb(d1,azm1,dip1,azt,dipt,dz,dn,de)
+            xt = xa+de
+            yt = ya+dn
+            zt = za+dz                    
+        end if
+  
+    end do
     
-    DEG2RAD=3.141592654/180.0
-    
-    i1 = (90 - dip1) * DEG2RAD
-    a1 = az1 * DEG2RAD
-    
-    dz = len12*cos(i1)
-    dn = len12*sin(i1)*cos(a1)
-    de = len12*sin(i1)*sin(a1)
-    
-    return
-    
-end subroutine dstang
+end subroutine desurv1dh
