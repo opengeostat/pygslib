5762fb8bdb16496997edc3f95c39b2868f7d9b11
opengeostat <adrian.martinez@opengeostat.com>
2016-06-28 21:55:43 -0700

    adding default file extension at grid2vtkfile

--- cython_code/vtktools.pyx
+++ cython_code/vtktools.pyx
@@ -805,6 +805,10 @@ cpdef grid2vtkfile(str path,
     
     
     """
+    
+    # add extension to path 
+    if not path.lower().endswith('.vtr'):
+        path = path + '.vtr'
 
     pyevtk.hl.gridToVTK(path, x, y, z, cellData=data)
 
