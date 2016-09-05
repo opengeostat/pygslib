f40a2b2dff08edc977242220b599a1f4d7b6d9b1
Adrian Martinez <Adrian@Beatrizs-iMac.local>
2016-09-05 00:16:54 -0700

    fixing conda package in OSX

--- pygslib_conda/pygslib_OSX/build.sh
+++ pygslib_conda/pygslib_OSX/build.sh
@@ -2,6 +2,9 @@
 
 conda config --add channels https://conda.anaconda.org/opengeostat
 
+# this is to fix error in conda build... see https://github.com/numpy/numpy/issues/7427
+LDFLAGS="$LDFLAGS -undefined dynamic_lookup -bundle"
+
 $PYTHON setup.py install
 
 # Add more build steps here, if they are necessary.
