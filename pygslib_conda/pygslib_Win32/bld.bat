dc22741ed64f9185738a1294abc9270c3b996381
opengeostat <adrian.martinez@opengeostat.com>
2016-09-27 14:42:14 -0700

    Win32 version added

---
+++ pygslib_conda/pygslib_Win32/bld.bat
@@ -0,0 +1,12 @@
+:: pip install pyevtk
+
+conda config --add channels https://conda.anaconda.org/opengeostat
+
+"%PYTHON%" setup.py config --compiler=mingw32 build --compiler=mingw32 install
+if errorlevel 1 exit 1
+
+:: Add more build steps here, if they are necessary.
+
+:: See
+:: http://docs.continuum.io/conda/build.html
+:: for a list of environment variables that are set during the build process.
