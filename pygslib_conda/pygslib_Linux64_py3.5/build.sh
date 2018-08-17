02b40d31b535a7b23463568728b8150a5e9e3fca
Adrian Martinez <Adrian.Martinez@csaglobal.com>
2018-08-17 12:15:11 -0400

    conda packages for windows updated

--- pygslib_conda/pygslib_Linux64_py3.5/build.sh
+++ pygslib_conda/pygslib_Linux64_py3.5/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+conda config --add channels https://conda.anaconda.org/opengeostat
+
 pip install --no-deps /home/adrian/pygslib/dist/pygslib-0.0.0.3.9-cp35-cp35m-linux_x86_64.whl
 
 # Add more build steps here, if they are necessary.
---
+++ pygslib_conda/pygslib_Win32_py2.7/bld.bat
@@ -0,0 +1,6 @@
+
+conda config --add channels https://conda.anaconda.org/opengeostat
+
+pip install --no-deps c:\OG_Python\pygslib\dist\pygslib-0.0.0.3.9-cp36-cp36m-win_amd64.whl
+
+if errorlevel 1 exit 1
