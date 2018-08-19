8d97b5e88aa913f5417456f254d18a68596d02c5
Adrian Martinez Vargas <adrian.martinez@opengeostat.com>
2018-08-18 20:04:23 -0400

    conda pack updated

--- pygslib_conda/pygslib_OSX/build.sh
+++ pygslib_conda/pygslib_OSX/build.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
 conda config --add channels https://conda.anaconda.org/opengeostat
-conda config --add channels menpo
 
-# this is to fix error in conda build... see https://github.com/numpy/numpy/issues/7427
-LDFLAGS="$LDFLAGS -undefined dynamic_lookup -bundle"
-
-$PYTHON setup.py install
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
+pip install --no-deps /home/adrian/pygslib/dist/ #### update this
--- pygslib_conda/pygslib_Win32_py2.7/bld.bat
+++ pygslib_conda/pygslib_Win32_py2.7/bld.bat
@@ -1,6 +1,4 @@
 
-conda config --add channels https://conda.anaconda.org/opengeostat
-
-pip install --no-deps c:\OG_Python\pygslib\dist\pygslib-0.0.0.3.9-cp36-cp36m-win_amd64.whl
+pip install --no-deps C:\OG_Python\pygslib\pygslib_conda\wheels\pygslib-0.0.0.3.9-cp27-cp27m-win_amd64.whl
 
 if errorlevel 1 exit 1
