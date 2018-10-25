4c94dba7d04d6c57959c634848412da636eefab3
Adrian Martinez <Adrian@Beatrizs-iMac.local>
2018-10-25 15:04:06 -0400

    conda package for OSX updated

--- pygslib_conda/pygslib_OSX_py2.7/build.sh
+++ pygslib_conda/pygslib_OSX_py2.7/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 conda config --add channels https://conda.anaconda.org/opengeostat
+conda config --add channels conda-forge 
 
-pip install --no-deps /home/adrian/pygslib/dist/<filename.whl>
+pip install --no-deps ~/Documents/OG_Python/pygslib/pygslib_conda/wheels/pygslib-0.0.0.4.0.0-cp27-cp27m-macosx_10_6_x86_64.whl
