75a32f644d215be9da39289664831df066eadbb6
Adrian Martinez <Adrian.Martinez@csaglobal.com>
2018-10-25 12:24:18 -0400

    updating conda distributions

--- pygslib_conda/pygslib_Win64_py2.7/bld.bat
+++ pygslib_conda/pygslib_Win64_py2.7/bld.bat
@@ -1,4 +1,4 @@
 
-pip install --no-deps C:\OG_Python\pygslib\pygslib_conda\wheels\pygslib-0.0.0.3.9-cp27-cp27m-win_amd64.whl
+pip install --no-deps C:\OG_Python\pygslib\pygslib_conda\wheels\pygslib-0.0.0.4.0.0-cp27-cp27m-win_amd64.whl
 
 if errorlevel 1 exit 1
