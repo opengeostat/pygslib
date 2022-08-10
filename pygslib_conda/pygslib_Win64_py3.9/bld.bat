d75ee1f3fdd320e3ab4f42796c1e00632b0a1eba
Adrian Martinez Vargas <Adrian.Martinez@agnicoeagle.com>
2022-08-10 01:06:48 -0400

    updated onda build

--- pygslib_conda/pygslib_Win64_py3.9/bld.bat
+++ pygslib_conda/pygslib_Win64_py3.9/bld.bat
@@ -1,4 +1,4 @@
 
-pip install --no-deps C:\OG_Python\pygslib\pygslib_conda\wheels\pygslib-0.0.0.7.0.0-cp39-cp39-win_amd64.whl
+pip install --no-deps C:\OG_Python\pygslib\pygslib_conda\wheels\pygslib-0.0.0.7.1.0-cp39-cp39-win_amd64.whl
 
 if errorlevel 1 exit 1
